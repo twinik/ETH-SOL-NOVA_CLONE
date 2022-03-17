715505c22f009bcb30af80f5a92ce8b1a5dc8200
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-17 13:24:41 -0300

    production

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,20 @@ import Head from "next/head";
 import styles from "./index.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import React, { useState, useEffect } from "react";
-import { Intro,FeatureProject,Description} from "../components/Landing";
+import {
+  Intro,
+  FeatureProject,
+  Description,
+  Production,
+} from "../components/Landing";
 import Particles from "../components/Particles";
 export default function Home() {
   return (
     <div>
       <Intro />
-      <FeatureProject/>
-      <Description/>
+      <FeatureProject />
+      <Description />
+      <Production />
       <style jsx global>{`
         html,
         body {
