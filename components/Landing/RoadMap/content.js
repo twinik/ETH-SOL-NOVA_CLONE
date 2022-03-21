e289085b4883b324ed1eb0aa69782d86e80b5a72
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-21 01:41:22 -0300

    mpawd

---
+++ components/Landing/RoadMap/content.js
@@ -0,0 +1,30 @@
+export const steps = [
+  {
+    title: "Intro",
+    subtitle: "Setup",
+    content: ["Meet our team", "Get your project's details", "Plan & Scope"],
+  },
+  {
+    title: "Configure",
+    subtitle: "Demo",
+    content: ["Smart contract on Devnet", "Template setup", "Testing & QA"],
+  },
+  {
+    title: "Launch",
+    subtitle: "Initial Mint",
+    content: [
+      "Online & Available During Mint",
+      "Full technical support",
+      "Cross promo & marketing",
+    ],
+  },
+  {
+    title: "Follow Through",
+    subtitle: "Post Mint",
+    content: [
+      "Introductions to Marketplaces",
+      "Guidance & Listing Support",
+      "Strategic advice & dev support",
+    ],
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       <Design />
       <Audience />
       <Structure />
-
+      
       <style jsx global>{`
         html,
         body {
