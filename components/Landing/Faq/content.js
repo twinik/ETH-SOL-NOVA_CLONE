9b8c3b8e141be81046e89931c3a7f39a915a6a27
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-22 01:36:45 -0300

    xd

---
+++ components/Landing/Faq/content.js
@@ -0,0 +1,76 @@
+export const tabs = [
+  {
+    title: "General",
+    content: [
+      {
+        question: "How can you ensure distribution of mints is random?",
+        answer:
+          "We will be using a smart contract deployed on the Solana chain to distribute your NFTs. The tokens can be verified on the blockchain and our smart contract code can be audited by the Solana core devs.",
+      },
+      {
+        question: "What cryptocurrencies do you support minting for?",
+        answer: "We focus solely on Solana's blockchain.",
+      },
+      {
+        question: "What am I getting for the revenue share fee?",
+        answer:
+          " Nova Launch is not just providing development work. Of course, that component is on us & we offer full service support from the frontend website - to mint & smart contract & more. But we are equally invested in projects we take on, and put our marketing & relationships behind them - from introductions to marketplaces, influencers, & more - to signal boosting & collaborations with our own network.\n Partnering with Nova Launch is a decision to give your project the best chance of success in the NFT world.",
+      },
+      {
+        question: "Is your fee negotiable?",
+        answer:
+          "We quote a baseline 20% revenue share model in our website, but are willing to discuss with projects on a case-by-case basis. A lot of consideration is given to the economics of the mint, etc. \n We do not choose to take on every project due to our time limitations.",
+      },
+    ],
+  },
+  {
+    title: "About NFTs",
+    content: [
+      {
+        question: "What is an NFT?",
+        answer:
+          "Non-Fungible Tokens are unique, easily verifiable digital assets that can represent items such as GIFs, images, videos, music albums, and more. Anything that exists online can be purchased as an NFT, theoretically. \n NFTs are different from cryptocurrencies because they’re not interchangeable. Think of Pokémon cards: You can trade them, but a Gastly is not the same as a holographic Charizard. But a bitcoin is indistinguishable from another bitcoin.",
+      },
+      {
+        question: "Why would I want to own an NFT? Can I make money on it?",
+        answer:
+          "One reason to buy an NFT is for its emotional value, which isn’t so different from physical objects...unless you’re a total utilitarian. No one buys lip gloss because they need it. They buy it for the way it makes them feel. The same can be true for a GIF, image, video, or other digital asset. \n The other reason is because you think it’s valuable...and will only increase in value. And yes, you can make money off of an NFT by buying and reselling it for more.",
+      },
+      {
+        question: "How do you know your NFT is authentic?",
+        answer:
+          "NFT ownership is recorded on the blockchain, and that entry acts as a digital pink slip. Our NFTs will be encoded using Metaplex's smart contract on the Solana Blockchain.",
+      },
+      {
+        question: "Which wallets will I be able to use to mint an NFT?",
+        answer:
+          "We offer a number of wallet connections for minting, but Phantom is the recommended wallet.",
+      },
+    ],
+  },
+  {
+    title: "About Solana",
+    content: [
+      {
+        question: "What is the Solana Foundation?",
+        answer:
+          "The Solana Foundation is dedicated to growing the Solana network into the world’s most decentralized and censorship-resistant blockchain. The Foundation continues to invest significant resources in figuring out how to meet these objectives both now and in the future.",
+      },
+      {
+        question: "What is Proof-of-History?",
+        answer:
+          "Proof-of-History (PoH) is derived from Proof-of-Stake (PoS) and is one of the core innovations of Solana. It focuses on the concept that instead of trusting timestamps given on a block, you could prove that a message occurred at a specific time before and after an action. In distributed networks, agreeing on the time and sequence in which events happened is a challenge because nodes in a network can’t simply assume that an external source of time or timestamp appears in a message and is truthful. However, Solana’s Proof-of-History protocol makes it possible to create a historical record that proves an event that happened at a specific moment in time.",
+      },
+      {
+        question: "What is Metaplex?",
+        answer:
+          "Metaplex is an on-chain NFT framework that lets artists and creators launch their own self-hosted NFT storefronts as quickly as building a website. The platform provides ultra-fast NFTs with insanely low minting fees powered by Solana.",
+      },
+      {
+        question: "Why use Solana for NFTs?",
+        answer:
+          "Compared to ETH & other blockchain networks with an NFT ecosystem, Solana is superior in terms of predictable, extremely low (fractions of a penny) network fees & extremely fast true instant live minting. Click mint, send the transaction, and seconds later, your NFT is now in your wallet. All possible with Nova Launch.",
+      },
+    ],
+  },
+];
