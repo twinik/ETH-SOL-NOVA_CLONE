cc58f676f0d10c2b8232886b1e77183f21a32c2b
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-22 17:14:01 -0300

    xd

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,20 @@ import "swiper/css";
 import "swiper/css/navigation";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer";
-
+import { AnimatePresence } from "framer-motion";
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <AnimatePresence
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        
+      >
+        <Component {...pageProps} />
+      </AnimatePresence>
       <Footer />
     </>
   );
