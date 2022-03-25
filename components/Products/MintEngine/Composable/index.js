f137d6b9a0437c423a0ba1f9588aaeac32e6bb2a
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-24 23:50:06 -0300

    composition

--- components/Products/MintEngine/Composable/index.js
+++ components/Products/MintEngine/Composable/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ownStyles from "./Composable.module.css";
 import { ContainerSection } from "../../../Containers";
-import { SecondaryTitle } from "../../../Titles";
-import { MainParagraph } from "../../../Paragraphs";
-import { ButtonRoundedSecondary } from "../../../Buttons";
+import { SecondaryTitle, LineTitleSecondary } from "../../../Titles";
+import { SecondaryParagraph } from "../../../Paragraphs";
+import { IconTextSecondary } from "../../../General";
 import Image from "next/image";
 export default function index() {
   return (
@@ -12,25 +12,6 @@ export default function index() {
       backgroundColor={"#571aaa"}
       first={true}
     >
-      <div className={ownStyles["content"]}>
-        <SecondaryTitle
-          style={{ whiteSpace: "pre-line" }}
-          text={"Secure, Worry-free \n Live Minting"}
-        />
-        <MainParagraph
-          style={{ textAlign: "center", color: "#af9cff" }}
-          content={
-            "No coding required. Let Nova Launch - the trusted partner in high volume, best-in-class NFT live minting on the  Solana network handle your smart contract needs."
-          }
-        />
-        <ButtonRoundedSecondary
-          text={"Mint with Nova Launch"}
-          style={{
-            background:
-              "linear-gradient(51deg, rgba(126,115,248,1) 0%, rgba(49,195,185,1) 99%)",
-          }}
-        />
-      </div>
       <div className={ownStyles["image"]}>
         <Image
           width={385}
@@ -40,6 +21,23 @@ export default function index() {
           layout="responsive"
           src={"/assets/nova-launch-dashboard.png"}
         />
+      </div>
+      <div className={ownStyles["content"]}>
+        <LineTitleSecondary text={"COMPOSABLE SMART CONTRACT"} />
+        <SecondaryTitle text={"Setup a Mint, easily."} />
+        <SecondaryParagraph
+          style={{ textAlign: "center", color: "#af9cff" }}
+          content={
+            "Create presales, live minting, gated mints & more through our platform."
+          }
+        />
+        <SecondaryParagraph
+          style={{ textAlign: "center", color: "#af9cff" }}
+          content={
+            "A completely seamless experience to launching your NFT project on Solana."
+          }
+        />
+        <IconTextSecondary text={"A Trusted, Production-Tested Smart Contract"} />
       </div>
     </ContainerSection>
   );
--- pages/products/mint-engine.js
+++ pages/products/mint-engine.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Intro, Mint } from "../../components/Products/MintEngine";
+import { Intro, Mint,Composable } from "../../components/Products/MintEngine";
 const Page = () => (
   <div>
     <Intro />
     <Mint />
+    <Composable />
     <style jsx global>{`
       body {
         background-color: #571aaa !important;
