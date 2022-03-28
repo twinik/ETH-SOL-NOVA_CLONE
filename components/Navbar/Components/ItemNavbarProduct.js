59a81319904f2ae9df201c3ef965d1a93b849c17
galo <67323030+gfalotico@users.noreply.github.com>
2022-03-28 14:03:24 -0300

    Update ItemNavbarProduct.js

--- components/Navbar/Components/ItemNavbarProduct.js
+++ components/Navbar/Components/ItemNavbarProduct.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./ItemNavbarProduct.module.css";
+import Link from "next/link";
 export default function ItemNavbarProduct({
 	title,
 	description,
@@ -9,21 +10,22 @@ export default function ItemNavbarProduct({
 	link
 }) {
 	return (
-		<a
-			href={link}
-			className={styles["ItemNavbarProduct-container"]}
-			style={style ? style : {}}
+		<Link href={link || ""} replace>
+		<div
+		  className={styles["ItemNavbarProduct-container"]}
+		  style={style ? style : {}}
 		>
-			<FontAwesomeIcon
-				icon={icon}
-				className={styles["ItemNavbarProduct-icon"]}
-			/>
-			<div className={styles["ItemNavbarProduct-content"]}>
-				<h4 className={styles["ItemNavbarProduct-title"]}>{title}</h4>
-				<p className={styles["ItemNavbarProduct-description"]}>
-					{description}
-				</p>
-			</div>
-		</a>
+		  <FontAwesomeIcon
+			icon={icon}
+			className={styles["ItemNavbarProduct-icon"]}
+		  />
+		  <div className={styles["ItemNavbarProduct-content"]}>
+			<h4 className={styles["ItemNavbarProduct-title"]}>{title}</h4>
+			<p className={styles["ItemNavbarProduct-description"]}>
+			  {description}
+			</p>
+		  </div>
+		</div>
+	  </Link>
 	);
 }
